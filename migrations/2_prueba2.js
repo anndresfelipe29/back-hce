f3bdfdb8a33354ee22885a49b40a731663261426
anndresfelipe29 <afgomezs@correo.udistrital.edu.co>
2023-01-14 18:29:26 -0500

    Se ajusta initial script

--- migrations/2_prueba2.js
+++ migrations/2_prueba2.js
@@ -1,31 +1,44 @@
 // const PersonaStruct = artifacts.require("PersonaStruct");
 // const PacienteDAO = artifacts.require("PacienteDAO");
 // const PacienteService = artifacts.require("PacienteService");
-
+console.log("==================PacienteMapper====================")
+const PacienteMapper = artifacts.require("PacienteMapper");
+console.log("==================Paciente====================")
+const Paciente = artifacts.require("Paciente");
+// const PacienteVO = artifacts.require("PacienteVO");
+console.log("==================MedicoMapper====================")
+const MedicoMapper = artifacts.require("MedicoMapper");
+console.log("==================Medico====================")
+const Medico = artifacts.require("Medico");
+console.log("==================Acceso====================")
+const Acceso = artifacts.require("Acceso");
+console.log("==================EstadoVO====================")
+// const PermisoRolMapper = artifacts.require("PermisoRolMapper");
+const EstadoVO = artifacts.require("EstadoVO");
+console.log("==================UsuarioMapper====================")
+const UsuarioMapper = artifacts.require("UsuarioMapper");
+console.log("==================RolMapper====================")
+const RolMapper = artifacts.require("RolMapper");
+console.log("==================TipoIdentificacionVO====================")
+const TipoIdentificacionVO = artifacts.require('TipoIdentificacionVO');
+console.log("==================DatosParametricosMapper====================")
+const DatosParametricosMapper = artifacts.require('DatosParametricosMapper');
+// const MedicoOraculo = artifacts.require('MedicoOraculo')
+console.log("==================HistoriaClinica====================")
+const HistoriaClinica = artifacts.require('HistoriaClinica')
+console.log("==================HistoriaClinicaMapper====================")
+const HistoriaClinicaMapper = artifacts.require('HistoriaClinicaMapper')
+console.log("==================AccesoHistoriaClinicaMapper====================")
+const AccesoHistoriaClinicaMapper = artifacts.require('AccesoHistoriaClinicaMapper')
+console.log("==================AccesoHistoriaClinica====================")
+const AccesoHistoriaClinica = artifacts.require('AccesoHistoriaClinica')
+console.log("==================Utils====================")
+const Utils = artifacts.require('Utils')
+console.log("==================Oracle====================")
+const Oracle = artifacts.require('Oracle')
 
 module.exports = function (deployer) {
     try {
-
-        const PacienteMapper = artifacts.require("PacienteMapper");
-        const Paciente = artifacts.require("Paciente");
-        // const PacienteVO = artifacts.require("PacienteVO");
-        const MedicoMapper = artifacts.require("MedicoMapper");
-        const Medico = artifacts.require("Medico");
-        const Acceso = artifacts.require("Acceso");
-        // const PermisoRolMapper = artifacts.require("PermisoRolMapper");
-        const EstadoVO = artifacts.require("EstadoVO");
-        const UsuarioMapper = artifacts.require("UsuarioMapper");
-        const RolMapper = artifacts.require("RolMapper");
-        const TipoIdentificacionVO = artifacts.require('TipoIdentificacionVO');
-        const DatosParametricosMapper = artifacts.require('DatosParametricosMapper');
-        // const MedicoOraculo = artifacts.require('MedicoOraculo')
-        const HistoriaClinica = artifacts.require('HistoriaClinica')
-        const HistoriaClinicaMapper = artifacts.require('HistoriaClinicaMapper')
-        const AccesoHistoriaClinicaMapper = artifacts.require('AccesoHistoriaClinicaMapper')
-        const AccesoHistoriaClinica = artifacts.require('AccesoHistoriaClinica')
-        const Utils = artifacts.require('Utils')
-
-        const Oracle = artifacts.require('Oracle')
         // deployer.deploy(PersonaStruct);
         // deployer.deploy(PacienteService);
         deployer.deploy(PacienteMapper);
