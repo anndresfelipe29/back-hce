4b055bc1166457c916f3ec857c81cc4fedb7ad10
anndresfelipe29 <afgomezs@correo.udistrital.edu.co>
2023-01-14 15:43:17 -0500

    se elimina ajusta ip de la red

--- truffle-config.js
+++ truffle-config.js
@@ -43,7 +43,7 @@ module.exports = {
     //
     development: {
       // host: 'localhost',
-      host: '172.19.0.1',
+      host: '172.18.0.1',
       port: 7545,
       //port: 8545,
       network_id: '*',   // * para que se envie la petición a cualquier red en esa dirección
